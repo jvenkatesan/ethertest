fd3096c1eec0a558ce86070aedf9e7254cf206da
jv <jv_400@yahoo.com>
2018-01-11 02:21:26 +0530

    default
    default

---
+++ utils.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.16;
+
+library Util{
+    
+    struct DateTime{
+        uint day;
+        uint month;
+        uint year;
+        uint hour;
+        uint minute;
+        uint second;
+    }
+    
+    
+}
